4a7277b75e60eb2701c35939e227d22c8cf352b9
RefilweMk3 <refilwekgwadi13@gmail.com>
2024-11-08 22:03:48 +0200

    First Set Bit

---
+++ main.py
@@ -0,0 +1,17 @@
+def find_rightmost_set_bit_position(n):
+    if n == 0:
+        return -1 
+    position = 1  
+    while (n & 1) == 0: 
+        n = n >> 1 
+        position += 1  
+
+    return position
+
+number = int(input("Enter number: "))
+position = find_rightmost_set_bit_position(number)
+
+if position == -1:
+    print("There are no set bits in the number.")
+else:
+    print("Position of the first set bit:", position)
